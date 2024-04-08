97a244f82c24b2f473a1a65a61a7c7f6140327c9
CodyMoltar <66773011+CodyMoltar@users.noreply.github.com>
2024-04-08 09:44:01 +0300

    updated example

---
+++ 2023/Basics of Interactivity for New media/5. p5-interactivity/sketch.js
@@ -0,0 +1,37 @@
+let img;
+
+// Load the image and create a p5.Image object.
+function preload() {
+    img = loadImage("./photo.jpg");
+}
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    image(img, 0, 0, width, height);
+}
+
+function draw() {
+    noFill();
+    strokeWeight(5);
+
+    if (mouseX > width / 2 - 60) {
+        if (mouseX < width / 2 - 60 + 140) {
+            if (mouseY > 50) {
+                if (mouseY < 50 + 140) {
+                    circle(random(width), random(height), 25);
+                }
+            }
+        }
+    } else {
+        image(img, 0, 0, width, height);
+    }
+
+    // rect(width / 2 - 60, 50, 140);
+
+    // if (mouseX < width / 2 && mouseY < height / 2) {
+    //     circle(mouseX, mouseY, 50);
+    // }
+    // if (mouseX > width / 2 && mouseY > height / 2) {
+    //     square(mouseX, mouseY, 50);
+    // }
+}
