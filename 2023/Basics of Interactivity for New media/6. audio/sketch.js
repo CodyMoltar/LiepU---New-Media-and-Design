f92816da0410a6ea92cfd716a8ed299dc30f4f8d
CodyMoltar <66773011+CodyMoltar@users.noreply.github.com>
2024-04-08 13:36:17 +0300

    added distance to circle example

--- 2023/Basics of Interactivity for New media/6. audio/sketch.js
+++ 2023/Basics of Interactivity for New media/6. audio/sketch.js
@@ -1,59 +1,85 @@
+let userHasClicked = false;
+
 let riverSound;
 let metalSound;
-let userHasClicked = false;
+
+let circleX = 200;
+let circleY = 300;
+
 
 
 function preload() {
   riverSound = loadSound('assets/river.wav');
-  metalSound = loadSound('assets/metal.wav');
+  metalSound = loadSound('assets/metal.wav')
 }
 
-
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    textAlign(CENTER);
-    riverSound.setVolume(1)
+  createCanvas(windowWidth, windowHeight);
+  textSize(30)
+  textAlign(CENTER)
+
+  riverSound.setVolume(0)
+  metalSound.setVolume(0.5)
+
 }
 
 function draw() {
-    background("hotpink");
+  let metalColor = color('red');
+  let riverColor = color('green');
 
-    if(userHasClicked == false){
-      text('Click to start, or something.. idk', width/2, height/2)
-    }
-    else{
-      
-      // put all the code after the user has clicked
+  background('hotpink');
 
-      if(riverSound.isPlaying() == false){
-        riverSound.play();
-      }
+  if(userHasClicked == false){
+    text(
+      'hi, click to continue, or something, idk...',
+      width/2,
+      height/2  
+    )
+  } else {
 
-      if(metalSound.isPlaying() == false){
-        metalSound.play();
-      }
-
-      let targetVolumeRiver = map(
-        mouseX,
-        0,
-        width,
-        0,
-        1
-      )
-      riverSound.setVolume(targetVolumeRiver)
-
-      let targetVolumeMetal = map(
-        mouseX,
-        0,
-        width,
-        1,
-        0
-      )
-      metalSound.setVolume(targetVolumeMetal)
-
+    if(riverSound.isPlaying() == false){
+      riverSound.play();
     }
 
-    
+    if(metalSound.isPlaying() == false){
+      metalSound.play();
+    }
+
+    let targetVolumeRiver = map(
+      mouseX,
+      0,
+      width,
+      0,
+      1
+    )
+    riverSound.setVolume(0)
+
+    let targetVolumeMetal = map(
+      mouseX,
+      0,
+      width,
+      1,
+      0
+    )
+    metalSound.setVolume(0)
+
+    let targetColor = lerpColor(riverColor, metalColor, targetVolumeRiver)
+    background(targetColor)
+
+    let distanceToMouse = dist(mouseX, mouseY, circleX, circleY);
+    let newVolume = map(distanceToMouse, 0, 150, 1, 0)
+    newVolume = constrain(newVolume, 0 , 1);
+
+    // circle(circleX, circleY, 50)
+
+    riverSound.setVolume(newVolume)
+
+  }
+  
+  
+
+  
+
 }
 
 function mouseClicked(){
